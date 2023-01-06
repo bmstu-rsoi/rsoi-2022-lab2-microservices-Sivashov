2a42ff89527a51e482799850f06ec1bbd39136e7
Sivashov <ya.siv11@yandex.ru>
2023-01-06 23:06:43 +0300

    Fixed data json

--- src/gateway/index.js
+++ src/gateway/index.js
@@ -182,8 +182,8 @@ app.get('/api/v1/privilege', async function (req, res) {
   const history_data = await getBonusHistory(bonus_data.data[0].id)
   console.log("History: ", history_data.data)
   if (history_data.data) {
-    let dat = {date: history_data.data[0].datetime, ticketUid: history_data.data[0].ticket_uid,
-                balanceDiff: history_data.data[0].balance_diff, operationType: history_data.data[0].operation_type}
+    let dat = {date: history_data.data[1].datetime, ticketUid: history_data.data[1].ticket_uid,
+                balanceDiff: history_data.data[1].balance_diff, operationType: history_data.data[1].operation_type}
     /*let dat2 = {date: history_data.data[1].datetime, ticketUid: history_data.data[1].ticket_uid,
                   balanceDiff: history_data.data[1].balance_diff, operationType: history_data.data[1].operation_type}*/
     res.status(200).json({balance: bonus_data.data[0].balance, status: bonus_data.data[0].status, history: [dat]})
