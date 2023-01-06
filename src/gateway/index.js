d58edc54e4d7b954edca8de020c067c454cb9e59
Sivashov <ya.siv11@yandex.ru>
2023-01-07 00:12:28 +0300

    Upd

--- src/gateway/index.js
+++ src/gateway/index.js
@@ -183,12 +183,13 @@ app.get('/api/v1/privilege', async function (req, res) {
   console.log("History 1: ", history_data.data)
   console.log("History 2: ", history_data.data)
   let datt = history_data.data[0]
-  let datd = (await history_data).data[1]
+  let obj = history_data.data.find(o => o.id === 2);
+  console.log("Obj: ", obj)
   let dat = {date: datt.datetime, ticketUid: datt.ticket_uid,
                 balanceDiff: datt.balance_diff, operationType: datt.operation_type}
-  /*let dat2 = {date: "", ticketUid: datd["ticket_uid"],
-                  balanceDiff: datd["balance_diff"], operationType: datd["operation_type"]}*/
-  res.status(200).json({balance: bonus_data.data[0].balance, status: bonus_data.data[0].status, history: [dat, dat]})
+  let dat2 = {date: "", ticketUid: obj["ticket_uid"],
+                  balanceDiff: obj["balance_diff"], operationType: obj["operation_type"]}
+  res.status(200).json({balance: bonus_data.data[0].balance, status: bonus_data.data[0].status, history: [dat, dat2]})
   });
 
 app.get('/api/v1/tickets', async function (req, res) {
